1d74f93405e25db946e5aa78ea748346fce30fbf
Niels van Galen Last <nvangalenlast@gmail.com>
2017-02-22 00:37:40 -0800

    added option to install nvm instead of nodenv

--- node/install.sh
+++ node/install.sh
@@ -5,18 +5,32 @@
 
 function install_nodenv() {
   if ! brew_test_package 'nodenv'; then
-    ask_for_confirmation "Do you want to install Nodenv?"
-    if answer_is_yes; then
-      brew tap nodenv/nodenv
-      brew update
-      brew install nodenv nodenv/nodenv/nodenv-package-rehash
-    fi
+    brew tap nodenv/nodenv
+    brew update
+    brew install nodenv nodenv/nodenv/nodenv-package-rehash
     print_success "Nodenv has been installed. Please run 'nodenv init' and follow the instructions."
+  else
+    print_success "Nodenv was already installed. Please run 'nodenv init' and follow the instructions."
   fi
 }
 
 function main() {
-  install_nodenv
+  print_info "You can install and manage node with nodenv or nvm"
+  ask_for_confirmation "Do you want to install nodenv?"
+  if answer_is_yes; then
+    install_nodenv
+  fi
+  if ! brew_test_package 'nodenv'; then
+    ask_for_confirmation "Do you want to install nvm?"
+    if answer_is_yes; then
+      # ...
+      # curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+      curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.1/install.sh | bash
+      print_success "nvm has been installed and added to your shell config."
+    fi
+  else
+    print_error "Unable to install nvm, please remove nodenv first."
+  fi
 }
 
 if [ "${1}" != "--source-only" ]; then
