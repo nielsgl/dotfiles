73064b2633fede9450819f2ef96745a45e8fe73a
Niels van Galen Last <nvangalenlast@gmail.com>
2017-04-01 20:24:00 +0200

    updated gitconfig.local script

--- git/setup.sh
+++ git/setup.sh
@@ -5,8 +5,9 @@
 
 function setup_gitconfig () {
   ask_for_confirmation "Do you want to generate gitconfig file?"
+  rootdir="$(cd "$(dirname "$BASH_SOURCE")"; cd ..; pwd)"
   if answer_is_yes; then
-    if ! [ -f git/gitconfig.local.symlink ]
+    if ! [ -f $rootdir/git/gitconfig.local.symlink ]
     then
       print_info 'Setting up gitconfig.'
 
@@ -19,7 +20,7 @@ function setup_gitconfig () {
       ask "What is your github author name?" git_authorname
       ask 'What is your github author email?' git_authoremail
 
-      sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" git/gitconfig.local.symlink.example > git/gitconfig.local.symlink
+      sed -e "s/AUTHORNAME/$git_authorname/g" -e "s/AUTHOREMAIL/$git_authoremail/g" -e "s/GIT_CREDENTIAL_HELPER/$git_credential/g" $rootdir/git/gitconfig.local.symlink.example > $rootdir/git/gitconfig.local.symlink
 
       print_success 'Setting up gitconfig complete.'
     fi
